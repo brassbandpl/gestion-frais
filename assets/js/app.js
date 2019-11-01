f945357e42ac9749e1c45308186d1e981e4ef3fb
Louis-Marie Gaborit <lm.gabo@gmail.com>
2019-11-01 23:25:53 +0100

    Symfony Update + Replace symfony/webpack-encore-pack with symfony/webpack-encore-bundle

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,6 +4,9 @@ const $ = require('jquery');
 // JS is equivalent to the normal "bootstrap" package
 // no need to set this to a variable, just require it
 require('bootstrap');
+require('../css/global.scss');
+require('../css/main.css');
+require('../css/util.css');
 
 // or you can include specific pieces
 // require('bootstrap/js/dist/tooltip');
