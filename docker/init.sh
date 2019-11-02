c613a8638f484f557d4db48b3c72f9b088296072
Louis-Marie Gaborit <lm.gabo@gmail.com>
2019-11-02 16:10:32 +0100

    Fix : start app on docker

--- docker/init.sh
+++ docker/init.sh
@@ -1,3 +1,13 @@
 #bin/bash
 #!/usr/bin/env bash
-php bin/console server:run 0.0.0.0:8000
+if [ ! -d "vendor" ]; then
+  composer install
+fi
+if [ ! -d "node_modules" ]; then
+  yarn install
+fi
+
+yarn encore dev
+php bin/console doctrine:schema:update --force
+php bin/console doctrine:fixtures:load
+php bin/console server:run *:8000
