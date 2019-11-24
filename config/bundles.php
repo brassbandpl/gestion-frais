19f2b1a7ff8caf33b4984fb0daed04b230fd4b87
Louis-Marie Gaborit <lm.gabo@gmail.com>
2019-11-24 18:51:11 +0100

    Add pack template

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
