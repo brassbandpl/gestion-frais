83469002b945969c7a992f6ecbc8caf49033d2bf
Louis-Marie Gaborit <lm.gabo@gmail.com>
2019-12-03 13:33:19 +0100

    Composer sync-recipes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,18 @@
-// app.js
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
 
-const $ = require('jquery');
-// JS is equivalent to the normal "bootstrap" package
-// no need to set this to a variable, just require it
+// any CSS you require will output into a single css file (app.css in this case)
 require('bootstrap');
 require('../css/global.scss');
 require('../css/main.css');
 require('../css/util.css');
 
-// or you can include specific pieces
-// require('bootstrap/js/dist/tooltip');
-// require('bootstrap/js/dist/popover');
+// Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
+const $ = require('jquery');
 
 $(document).ready(function() {
     $('[data-toggle="popover"]').popover();
