79ea32e28799e377325b16d9a22011512ea7919d
Louis-Marie GABORIT <lm.gabo@gmail.com>
2020-11-29 15:54:44 +0100

    Update wording reset password mail

--- src/Controller/ResetPasswordController.php
+++ src/Controller/ResetPasswordController.php
@@ -160,9 +160,9 @@ class ResetPasswordController extends AbstractController
         }
 
         $email = (new TemplatedEmail())
-            ->from(new Address('support-ik@brassband-paysdelaloire.fr', 'Support IK BBPL'))
+            ->from(new Address('gestionfrais@brassband-paysdelaloire.fr', 'BBPL Gestion Frais'))
             ->to($user->getEmail())
-            ->subject('Your password reset request')
+            ->subject('Demande de réinitialisation de mot de passe')
             ->htmlTemplate('reset_password/email.html.twig')
             ->context([
                 'resetToken' => $resetToken,
